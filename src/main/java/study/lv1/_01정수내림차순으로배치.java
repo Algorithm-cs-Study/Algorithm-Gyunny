5893bedea2fa30bb1e90bc3ca1bfe3c556bb5415
gyunny <needle12s@gmail.com>
2023-02-17 15:13:05 +0900

    정수 내림차순으로 배치

---
+++ src/main/java/study/lv1/_01정수내림차순으로배치.java
@@ -0,0 +1,26 @@
+package study.lv1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _01정수내림차순으로배치 {
+
+    public static void main(String[] args) {
+        _01정수내림차순으로배치 t = new _01정수내림차순으로배치();
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(t.solution(n));
+    }
+
+    public long solution(long n) {
+        StringBuilder sb = new StringBuilder();
+        String[] s = String.valueOf(n).split("");
+        Arrays.sort(s);
+        for (String x : s) {
+            sb.append(x);
+        }
+
+        return Long.parseLong(sb.reverse().toString());
+    }
+
+}
---
+++ src/main/java/study/lv1/_02나머지1이되는수찾기.java
@@ -0,0 +1,26 @@
+package study.lv1;
+
+import java.util.Scanner;
+
+public class _02나머지1이되는수찾기 {
+
+    public static void main(String[] args) {
+        _02나머지1이되는수찾기 t = new _02나머지1이되는수찾기();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(t.solution(n));
+    }
+
+    public int solution(int n) {
+        int x = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                x = i;
+                break;
+
+            }
+        }
+        return x;
+    }
+
+}
