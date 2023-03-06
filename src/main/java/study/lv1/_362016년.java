0682a1235e6c6b66f7acc2e699bec694a5765476
gyunny <needle12s@gmail.com>
2023-03-06 19:12:07 +0900

    2016년

---
+++ src/main/java/study/lv1/_362016년.java
@@ -0,0 +1,19 @@
+package study.lv1;
+
+public class _362016년 {
+
+    public static void main(String[] args) {
+        _362016년 t = new _362016년();
+        int a = 5;
+        int b = 24;
+        System.out.println(t.solution(a, b));
+    }
+
+    public String solution(int a, int b) {
+        String[] week = {"", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+        String answer = "";
+        return answer;
+    }
+
+}
