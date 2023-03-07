a896d03af70a5c9b773418a9ee51a842567879a0
gyunny <needle12s@gmail.com>
2023-03-07 12:21:55 +0900

    ReName

--- src/main/java/study/lv1/_36_2016년.java
+++ src/main/java/study/lv1/_36_2016년.java
@@ -2,10 +2,10 @@ package study.lv1;
 
 import java.time.LocalDate;
 
-public class _362016년 {
+public class _36_2016년 {
 
     public static void main(String[] args) {
-        _362016년 t = new _362016년();
+        _36_2016년 t = new _36_2016년();
         int a = 5;
         int b = 24;
         System.out.println(t.solution(a, b));
