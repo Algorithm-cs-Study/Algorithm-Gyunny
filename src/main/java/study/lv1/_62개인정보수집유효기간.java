6c9848eb65b92662a15c948de6a5064b5695e0e4
gyunny <needle12s@gmail.com>
2023-03-24 14:23:17 +0900

    개인정보 수집 유효기간

--- src/main/java/study/lv1/_62개인정보수집유효기간.java
+++ src/main/java/study/lv1/_62개인정보수집유효기간.java
@@ -2,7 +2,6 @@ package study.lv1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class _62개인정보수집유효기간 {
@@ -12,41 +11,36 @@ public class _62개인정보수집유효기간 {
         String today = "2022.05.19";
         String[] terms = {"A 6", "B 12", "C 3"};
         String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
-        for (int x : t.solution(today, terms, privacies)) {
+        for (Integer x : t.solution(today, terms, privacies)) {
             System.out.print(x + " ");
         }
     }
 
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        int todayDayCounts = getDays(today);
+    public ArrayList<Integer> solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int todayCounts = getDays(today);
 
         Map<String, String> termMap = new HashMap<>();
-        for (String term : terms) {
-            String[] words = term.split(" ");
-            String alpha = words[0];
-            String month = words[1];
+        for (String x : terms) {
+            String[] term = x.split(" ");
+            String alpha = term[0];
+            String month = term[1];
 
             termMap.put(alpha, month);
         }
 
-        List<Integer> destroyList = new ArrayList<>();
-        int idx = 1;
-        for (String privacy : privacies) {
-            String[] words = privacy.split(" ");
-            String date = words[0];
-            String alpha = words[1];
+        int cnt = 1;
+        for (String x : privacies) {
+            String[] privacy = x.split(" ");
+            String date = privacy[0];
+            String alpha = privacy[1];
+
             int monthLimit = Integer.parseInt(termMap.get(alpha));
-
             int limitDayDayCount = getDays(date) + getDays(monthLimit);
-            if (todayDayCounts >= limitDayDayCount) {
-                destroyList.add(idx);
+            if (todayCounts >= limitDayDayCount) {
+                answer.add(cnt);
             }
-            idx++;
-        }
-
-        int[] answer = new int[destroyList.size()];
-        for (int i = 0; i < destroyList.size(); i++) {
-            answer[i] = destroyList.get(i);
+            cnt++;
         }
 
         return answer;
