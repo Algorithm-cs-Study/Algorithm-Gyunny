aec22b20ef67c918224324bf85d417982d79ae8c
taegyun1995 <needle12s@gmail.com>
2023-04-26 21:23:40 +0900

    멀리뛰기

---
+++ src/main/java/study/lv2/_17귤고르기.java
@@ -0,0 +1,25 @@
+package study.lv2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _17귤고르기 {
+
+    public static void main(String[] args) throws IOException {
+        _17귤고르기 t = new _17귤고르기();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        s.
+
+    }
+
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+}
