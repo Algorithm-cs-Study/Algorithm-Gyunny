5108609cdfd2cc09a5132da21134f19aa3fad817
taegyun1995 <needle12s@gmail.com>
2023-05-28 17:50:31 +0900

    주식

--- src/main/java/study2/스택_큐/_006주식.java
+++ src/main/java/study2/스택_큐/_006주식.java
@@ -27,15 +27,21 @@ public class _006주식 {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < p_len; i++) {
+            // 스택이 비지 않고, 현재 가격이 스택의 가장 위의 인덱스의 가격보다 작으면
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                // 가격이 떨어졌으니, 떨어지지 않은 기간을 계산하여 결과를 배열에 저장
                 answer[stack.peek()] = i - stack.peek();
+                // 계산이 끝난 인덱스는 스택에서 제거
                 stack.pop();
             }
+            // 현재 인덱스를 스택에 push
             stack.push(i);
         }
-
+        // 모든 가격을 확인한 후에도 스택에 남아있는 인덱스가 있다면,
         while (!stack.isEmpty()) {
+            // 끝까지 가격이 떨어지지 않았으니 전체 길이에서 현재 인덱스를 뺀 값이 떨어지지 않은 기간이 됨
             answer[stack.peek()] = prices.length - stack.peek() - 1;
+            // 계산이 끝난 인덱스는 스택에서 제거
             stack.pop();
         }
 
