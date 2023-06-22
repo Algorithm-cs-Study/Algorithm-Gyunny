b0cea1bfb85ed31843c7002ec832a52dfe8e6ba7
taegyun1995 <needle12s@gmail.com>
2023-06-22 17:39:58 +0900

    카펫

--- src/main/java/study2/완전탐색/_004카펫.java
+++ src/main/java/study2/완전탐색/_004카펫.java
@@ -1,2 +1,35 @@
-package study2.완전탐색;public class _004카펫 {
+package study2.완전탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _004카펫 {
+
+    public static void main(String[] args) throws IOException {
+        _004카펫 t = new _004카펫();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int b = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(Arrays.toString(t.solution(b, y)));
+    }
+
+    public int[] solution(int b, int y) {
+        int[] answer = new int[2];
+
+        for (int i = 3; i < b + y; i++) {
+            int width = (b + y) / i;
+            if (width >= i) {
+                if ((i - 2) * (width - 2) == y) {
+                    answer[0] = width;
+                    answer[1] = i;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
 }
